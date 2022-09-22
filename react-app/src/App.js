4486affe0fcff68c13d7302fecd5cfcfa42f4da9
Sávio Freire <freiresavio7@gmail.com>
2022-09-22 15:03:09 -0300

    sobre 100%

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
 import Header from './header/Header'
+import QuemSou from './quemSou/quemSou'
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <QuemSou />
     </div>
   );
 }
