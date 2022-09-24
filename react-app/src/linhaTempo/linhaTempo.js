078286291be5c65f2ab13ee77b4ef8b0f2947284
Sávio Freire <freiresavio7@gmail.com>
2022-09-24 11:56:25 -0300

    fotos

--- react-app/src/linhaTempo/linhaTempo.js
+++ react-app/src/linhaTempo/linhaTempo.js
@@ -5,7 +5,11 @@ import Epoca from './epoca';
 import loja from '../foto/loja.jpeg'
 import gta from '../foto/gta5.jpg'
 import escola from '../foto/escola.jpg'
-
+import convertido from '../foto/convertido.jpg'
+import conheci from '../foto/conheciAna.jpg'
+import culturaI from '../foto/culturaI.jpg'
+import estacio from '../foto/estacio.jpg'
+import danki from '../foto/danki.jpg'
 
 export default function LinhaTempo(){
 
@@ -14,10 +18,12 @@ export default function LinhaTempo(){
             <div className='linha'>
                 
             </div>
-            <Epoca class='epoca' ano='2011' sobre='Minha vida profissional começou cedo, com 13 anos eu comecei a trabalhar na loja de bike do meu pai' img={loja}/>
-            <Epoca class='epoca2' ano='2013' sobre='Lançamento do GTA 5, foi um jogo que me marcou bastante, lembro desse dia como se fosse ontem.' img={gta}/>
-            <Epoca class='epoca' ano='2015' sobre='Terminei o ensino medio sem saber o que eu queria fazer, na verdade, naquele momento não queria fazer nada.' img={escola}/>
-            <Epoca class='epoca2' ano='2018' sobre='' img={}/>
+            <Epoca class='epoca' ano='Jan/2011' sobre='Minha vida profissional começou cedo, com 13 anos eu comecei a trabalhar na loja de bike do meu pai.' img={loja}/>
+            <Epoca class='epoca2' ano='Jan/2017' sobre='Comecei o curso de Inglês no Cultura Inglesa. Aprendi uma boa base da lingua.' img={culturaI}/>
+            <Epoca class='epoca' ano='Mar/2022' sobre='Comecei a faculdade de Análise e Desenvolvimento de Sistemas na Universidade Estácio de Sá. ' img={estacio}/>
+            <Epoca class='epoca2' ano='Mai/2017' sobre='Decidir que queria me especializar na área de Desenvolvimento Front-End, Então comecei a fazer cursos na DankiCode. ' img={danki}/>
+            
+            
             
             
         </div>
