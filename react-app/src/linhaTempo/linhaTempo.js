0807051acb6c11f51191546fdd17566f256626a4
Sávio Freire <freiresavio7@gmail.com>
2022-09-28 16:42:56 -0300

    build 4

--- react-app/src/linhaTempo/linhaTempo.js
+++ react-app/src/linhaTempo/linhaTempo.js
@@ -2,6 +2,8 @@ import './linhaTempo.css';
 import '../gradient/gradient.css';
 import './epoca.css'
 
+import { useEffect, useState } from 'react';
+
 import MiniEpoca from './miniEpoca';
 import Epoca from './epoca';
 import loja from '../foto/loja.jpeg'
@@ -19,18 +21,32 @@ import danki from '../foto/danki.png'
 
 export default function LinhaTempo(){
 
+    const [aparecerLinhaTempo, setAparecerLinhaTempo] = useState(false);
+
+    useEffect(()=>{
+        const posicao = ()=>{
+            if(window.scrollY>650){
+                setAparecerLinhaTempo(true);
+            }
+            else{
+                setAparecerLinhaTempo(false)
+            }
+        }
+        window.addEventListener('scroll', posicao)
+    })
+
     return(
         <div className='linhaTempo'>
-            <div className='linha'>
+            <div className={aparecerLinhaTempo ? 'linha' : 'linhaApagada'}>
                 
             </div>
-            <Epoca class='epoca' ano='Jan/2011' sobre='Minha vida profissional começou cedo, com 13 anos eu comecei a trabalhar na loja de bicicletas do meu pai.' img={loja}/>
-            <Epoca class='epoca2' ano='Jan/2017' sobre='Comecei o curso de Inglês no Cultura Inglesa. Aprendi uma boa base da lingua.' img={ingles}/>    
-            <Epoca class='epoca' ano='Mar/2022' sobre='Comecei a faculdade de Análise e Desenvolvimento de Sistemas na Universidade Estácio de Sá. ' img={estacio}/>
-            <Epoca class='epoca2' ano='Mai/2022' sobre='Decidi que queria me especializar na área de Desenvolvimento Front-End.' img={front}/>
-            <Epoca class='epoca' ano='Jun/2022' sobre='Aprendi HTML e CSS nas aulas do do Curso em Vídeo e no curso de Front-End na DankiCode' img={htmlCss}/>
-            <Epoca class='epoca2' ano='Ago/2022' sobre='Aprendi JavaScript com o curso de JavaScript Puro da DankiCode' img={js}/>
-            <Epoca class='epoca' ano='Set/2022' sobre='Foi amor a primeira vista quando comecei a aprender ReactJs, realizei o curso de ReactJs da DankiCode' img={react}/>
+            <Epoca class={aparecerLinhaTempo ? 'epoca' : 'sumidoEpoca'} ano='Jan/2011' sobre='Minha vida profissional começou cedo, com 13 anos eu comecei a trabalhar na loja de bicicletas do meu pai.' img={loja}/>
+            <Epoca class={aparecerLinhaTempo ? 'epoca2' : 'sumidoEpoca2'} ano='Jan/2017' sobre='Comecei o curso de Inglês no Cultura Inglesa. Aprendi uma boa base da lingua.' img={ingles}/>    
+            <Epoca class={aparecerLinhaTempo ? 'epoca' : 'sumidoEpoca'} ano='Mar/2022' sobre='Comecei a faculdade de Análise e Desenvolvimento de Sistemas na Universidade Estácio de Sá. ' img={estacio}/>
+            <Epoca class={aparecerLinhaTempo ? 'epoca2' : 'sumidoEpoca2'} ano='Mai/2022' sobre='Decidi que queria me especializar na área de Desenvolvimento Front-End.' img={front}/>
+            <Epoca class={aparecerLinhaTempo ? 'epoca' : 'sumidoEpoca'} ano='Jun/2022' sobre='Aprendi HTML e CSS nas aulas do do Curso em Vídeo e no curso de Front-End na DankiCode' img={htmlCss}/>
+            <Epoca class={aparecerLinhaTempo ? 'epoca2' : 'sumidoEpoca2'} ano='Ago/2022' sobre='Aprendi JavaScript com o curso de JavaScript Puro da DankiCode' img={js}/>
+            <Epoca class={aparecerLinhaTempo ? 'epoca' : 'sumidoEpoca'} ano='Set/2022' sobre='Foi amor a primeira vista quando comecei a aprender ReactJs, realizei o curso de ReactJs da DankiCode' img={react}/>
             
             
             
